4c44457f6557e215bf289b32d0fa424a95fc3d87
unknown <nilphym@gmail.com>
2021-05-18 12:22:54 +0200

    Fix resize problem

--- app.js
+++ app.js
@@ -1,39 +1,13 @@
-let currentImage = 'image1'
+const loadProject = () => {
+  let currentImage = 'image1'
 
-document.getElementById('reset').addEventListener('click', () => {
-  snapfit.admix(document.getElementById(currentImage))
-})
+  document.getElementById('reset').addEventListener('click', () => {
+    snapfit.admix(document.getElementById(currentImage))
+  })
 
-document.getElementById('solve').addEventListener('click', () => {
-  snapfit.solve(document.getElementById(currentImage))
-})
-
-snapfit.add(document.getElementById(currentImage), {
-  mixed: true,
-  aborder: true,
-  polygon: true,
-  space: 20,
-  level: 1,
-  aimage: true,
-})
-
-document.getElementById('level').addEventListener('change', evt => {
-  snapfit.reset(
-    document.getElementById(currentImage),
-    parseInt(evt.currentTarget.options[evt.currentTarget.selectedIndex].value)
-  )
-  snapfit.admix(document.getElementById(currentImage))
-})
-
-document.getElementById('picture').addEventListener('change', evt => {
-  snapfit.remove(document.getElementById(currentImage))
-  document.getElementById(currentImage).style = 'display: none'
-
-  currentImage = `image${
-    evt.currentTarget.options[evt.currentTarget.selectedIndex].value
-  }`
-
-  document.getElementById(currentImage).style = ''
+  document.getElementById('solve').addEventListener('click', () => {
+    snapfit.solve(document.getElementById(currentImage))
+  })
 
   snapfit.add(document.getElementById(currentImage), {
     mixed: true,
@@ -43,10 +17,36 @@ document.getElementById('picture').addEventListener('change', evt => {
     level: 1,
     aimage: true,
   })
-})
+
+  document.getElementById('level').addEventListener('change', evt => {
+    snapfit.reset(
+      document.getElementById(currentImage),
+      parseInt(evt.currentTarget.options[evt.currentTarget.selectedIndex].value)
+    )
+    snapfit.admix(document.getElementById(currentImage))
+  })
+
+  document.getElementById('picture').addEventListener('change', evt => {
+    snapfit.remove(document.getElementById(currentImage))
+    document.getElementById(currentImage).style = 'display: none'
+
+    currentImage = `image${
+      evt.currentTarget.options[evt.currentTarget.selectedIndex].value
+    }`
+
+    document.getElementById(currentImage).style = ''
+
+    snapfit.add(document.getElementById(currentImage), {
+      mixed: true,
+      aborder: true,
+      polygon: true,
+      space: 20,
+      level: 1,
+      aimage: true,
+    })
+  })
+}
 
 setTimeout(() => {
-  if (document.getElementById(currentImage).tagName === 'IMG') {
-    location.reload()
-  }
-}, 1000)
+  loadProject()
+}, 500)
